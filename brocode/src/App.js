4ae46c42a592d0e2a6261df6e6935891c332f9cf
Ryan Howard <ryanghoward@protonmail.com>
2022-05-02 15:27:38 -0700

    routes

--- brocode/src/App.js
+++ brocode/src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
-import './index.css';
+import "./index.css";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Home from "./Pages/Home";
+import Projects from "./Pages/Projects";
+
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <h1>broCode</h1>
-  )
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
