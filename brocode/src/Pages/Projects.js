fbfb752866d624f29b1b2f46c467ec8a4241c068
Ryan Howard <ryanghoward@protonmail.com>
2022-05-03 10:25:49 -0700

    Pricing Cards

--- brocode/src/Pages/Projects.js
+++ brocode/src/Pages/Projects.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import Hero2 from "../Components/Hero2";
+import PricingCard from "../Components/PricingCard";
 
 const Projects = () => {
   return (
     <div>
       <Navbar />
-      <Hero2 heading='PROJECTS.' text='Check out some of our work' />
+      <Hero2 heading='PROJECTS.' text='Check out some of our work.' />
+      <PricingCard />
       <Footer />
     </div>
   );
