4c616458809644cbe88d1ca0be7262d27edb8477
Ryan Howard <ryanghoward@protonmail.com>
2022-05-03 16:41:02 -0700

    About

--- brocode/src/Components/AboutContent.js
+++ brocode/src/Components/AboutContent.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./AboutContent.css";
+import code4 from "../assets/code4.jpg";
+import code5 from "../assets/code5.jpg";
 
 const AboutContent = () => {
   return (
@@ -15,7 +17,16 @@ const AboutContent = () => {
           <button className='btn'>Contact</button>
         </Link>
       </div>
-      <div className='right'></div>
+      <div className='right'>
+        <div className='img-container'>
+          <div className='img-stack top'>
+            <img src={code4} className='img' alt='code' />
+          </div>
+          <div className='img-stack bottom'>
+            <img src={code5} className='img' alt='code' />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
