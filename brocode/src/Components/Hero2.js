4a88f5a65f9eba4ae516f97c163470cd7850e009
Ryan Howard <ryanghoward@protonmail.com>
2022-05-03 10:01:46 -0700

    save

---
+++ brocode/src/Components/Hero2.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Hero2.css";
+
+const Hero2 = () => {
+  return (
+    <div className='hero-img'>
+      <div className='heading'>
+        <h1>This is a heading</h1>
+        <p>This is a paragraph</p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero2;
--- brocode/src/Pages/Projects.js
+++ brocode/src/Pages/Projects.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
+import Hero2 from "../Components/Hero2";
 
 const Projects = () => {
   return (
     <div>
       <Navbar />
+      <Hero2 />
       <Footer />
     </div>
   );
