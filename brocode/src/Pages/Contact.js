998248fe6aea82548346c4831a8c6c90c3cd71ae
Ryan Howard <ryanghoward@protonmail.com>
2022-05-04 09:26:17 -0700

    Contact

--- brocode/src/Pages/Contact.js
+++ brocode/src/Pages/Contact.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 import Hero2 from "../Components/Hero2";
+import Form from "../Components/Form";
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
       <Hero2 heading='CONTACT.' text="Like what you see? Let's get to work." />
+      <Form />
       <Footer />
     </div>
   );
