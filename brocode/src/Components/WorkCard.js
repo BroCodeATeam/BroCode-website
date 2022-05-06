d648654cb696ddc4093389f2444af126d2f22168
Philip R McDavid <31264591+FftyShadesofCode@users.noreply.github.com>
2022-05-06 17:20:45 -0400

    Removing the Idea Folder

--- brocode/src/Components/WorkCard.js
+++ brocode/src/Components/WorkCard.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./WorkCard.css";
 import { NavLink } from "react-router-dom";
-// import WorkCardData from "./WorkData";
 
 const WorkCard = (props) => {
   return (
