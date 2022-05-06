a8d1d1261711009a3223132603b60c6352142240
Ryan Howard <ryanghoward@protonmail.com>
2022-05-06 15:43:17 -0700

    Dynamic page heading data added

--- brocode/src/Components/Hero2.js
+++ brocode/src/Components/Hero2.js
@@ -6,8 +6,8 @@ class Hero2 extends Component {
     return (
       <div className='hero-img'>
         <div className='heading'>
-          <h1>Heading</h1>
-          <p>Check out some of our work.</p>
+          <h1>{this.props.heading}</h1>
+          <p>{this.props.text}</p>
         </div>
       </div>
     );
--- brocode/src/Pages/Contact.js
+++ brocode/src/Pages/Contact.js
@@ -1,14 +1,14 @@
 import React from "react";
-import Footer from "../Components/Footer";
+// import Footer from "../Components/Footer";
 import Hero2 from "../Components/Hero2";
 import Form from "../Components/Form";
 
 const Contact = () => {
   return (
     <div>
-      <Hero2 />
+      <Hero2 heading='CONTACT.' text="Like what you see? Let's get to work." />
       <Form />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
